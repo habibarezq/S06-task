65bc5e5352d239ca0c216cce190b7e3f7532be98
habibarezq <habibarezq30@gmail.com>
2024-09-10 21:37:38 +0300

    Temp node changed

--- pkg_py/pkg_py/collector.py
+++ pkg_py/pkg_py/collector.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from example_interfaces.msg import Int8MultiArray , Float32MultiArray 
+from example_interfaces.msg import Float32 , String
 class aggregatorNode(Node):
     def __init__(self):
         super().__init__("collector")
-        self.subscriber_=self.create_subscription(Int8MultiArray,"Temperature",self.callback_temp,10)
-        self.subscriber2_=self.create_subscription(Float32MultiArray,"pressure",self.callback_pressure,10)
+        self.subscriber_temp_=self.create_subscription(Float32,"Temperature",self.callback_temp,10)
+        self.subscriber_pres_=self.create_subscription(Float32,"pressure",self.callback_pressure,10)
         self.get_logger().info("Data Collection Has been Started!!")
     def callback_temp(self,msg):
-        self.get_logger().info("Temperature is " + str(msg.data[1]) + "°C")
+        self.get_logger().info(f"Temperature is {msg.data:.2f} °C")
     def callback_pressure(self,msg):
-        self.get_logger().info(f"Pressure is {msg.data[1]:.2f} atm")
+        self.get_logger().info(f"Pressure is {msg.data:.2f} atm")
 def main(args=None):
     rclpy.init(args=args)
     node=aggregatorNode()
--- pkg_py/pkg_py/temp_node.py
+++ pkg_py/pkg_py/temp_node.py
@@ -7,14 +7,14 @@ class TemperatureNode(Node):
     def __init__(self):
         super().__init__("temp_node")
         self.publisher_=self.create_publisher(Float32,"Temperature",10)
-        self.error_status_=self.create_publisher(String,"Temperature_Error",10)
+        self.error_status_=self.create_publisher(String,"temperature_error",10)
         self.timer=self.create_timer(1,self.publish_temp)
         self.get_logger().info("Temprature has been published!!")
     def publish_temp(self):
         msg=Float32()
         msg.data=self.get_temperature() #temperature ranges from 10°C to 100°C
         self.publisher_.publish(msg)
-        
+
         if self.error():
             error_msg=String()
             error_msg.data="Error in Temperature Sensor"
