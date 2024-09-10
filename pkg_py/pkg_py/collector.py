125665e65edd7ce68f03b4988d7abfde21765ece
habibarezq <habibarezq30@gmail.com>
2024-09-10 20:13:19 +0300

    Added Error status to temp node

--- pkg_py/pkg_py/collector.py
+++ pkg_py/pkg_py/collector.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from example_interfaces.msg import Int8  #UInt8 is signed 8 bits integer
+from example_interfaces.msg import Int8MultiArray  
 class aggregatorNode(Node):
     def __init__(self):
         super().__init__("collector")
-        self.subscriber_=self.create_subscription(Int8,"Temperature",self.callback,10)
+        self.subscriber_=self.create_subscription(Int8MultiArray,"Temperature",self.callback,10)
         self.get_logger().info("Data Collection Has been Started!!")
     def callback(self,msg):
-        self.get_logger().info("Temperature is " + str(msg.data) + "°C")
+        if(msg.data[0]==1 ):
+            if(msg.data[1]>=10 % msg.data[1]<=100):
+                self.get_logger().info("Sensor is working  & Temperature is " + str(msg.data) + "°C")
+            else:
+                self.get_logger().info("Sensor is working but Temperature out of Range!!")
+        else:
+            self.get_logger().info("Sensor Failed!!")
 def main(args=None):
     rclpy.init(args=args)
     node=aggregatorNode()
--- pkg_py/pkg_py/temp_node.py
+++ pkg_py/pkg_py/temp_node.py
@@ -1,17 +1,18 @@
 # !/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from example_interfaces.msg import Int8
+from example_interfaces.msg import Int8MultiArray
 import random
 class TemperatureNode(Node):
     def __init__(self):
         super().__init__("temp_node")
-        self.publisher_=self.create_publisher(Int8,"Temperature",10)
+        self.publisher_=self.create_publisher(Int8MultiArray,"Temperature",10)
         self.timer=self.create_timer(5,self.publish_temp)
         self.get_logger().info("Temprature has been published!!")
     def publish_temp(self):
-        msg=Int8()
-        msg.data= random.randint(-50,50) #temperature ranges from -20°C to 50°C
+        msg=Int8MultiArray()
+        msg.data[0]=1 #error status
+        msg.data[1]= random.randint(10,100) #temperature ranges from 10°C to 100°C
         self.publisher_.publish(msg)
 def main(args=None):
     rclpy.init(args=args)
